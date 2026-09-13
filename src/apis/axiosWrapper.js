ca470a304cf506cc27963d0540ff39ce5e0eb095
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-09-13 16:34:01 +0530

    fix: add API version prefix to frontend requests

--- src/apis/axiosWrapper.js
+++ src/apis/axiosWrapper.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 const defaultHeader = {
-    "Content-Type": "application/json",
-    Accept : "application/json",
+  "Content-Type": "application/json",
+  Accept: "application/json",
 };
 
 export const axiosWrapper = axios.create({
-    baseURL : import.meta.env.VITE_BACKEND_URL,
-    withCredentials: true,
-    headers : { ...defaultHeader },
-})
+  baseURL: `${import.meta.env.VITE_BACKEND_URL}/api/v1`,
+  withCredentials: true,
+  headers: { ...defaultHeader },
+});
